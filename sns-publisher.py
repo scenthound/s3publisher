892e0fe80c8db30e0ff16f252f71d0176c4da711
scenthound <scenthound.sniff@gmail.com>
2018-11-02 16:05:58 +0000

    Added region as a required argument

--- sns-publisher.py
+++ sns-publisher.py
@@ -1,8 +1,10 @@
 import boto3
 import click
 
+aws_session = None
+
 def publishS3ContentsToTopic(bucket, topic_arn):
-    snsclient = boto3.client('sns')
+    snsclient = aws_session.client('sns')
 
     for obj in listS3Objects(bucket):
         msg = readS3Object(bucket, obj)
@@ -13,22 +15,25 @@ def publishS3ContentsToTopic(bucket, topic_arn):
         print("Published: " + obj)
 
 def readS3Object(bucket, object):
-    s3 = boto3.resource('s3')
+    s3 = aws_session.resource('s3')
     obj = s3.Object(bucket, object)
     return obj.get()['Body'].read().decode('utf-8')
 
 def listS3Objects(bucket):
-    s3 = boto3.resource('s3')
+    s3 = aws_session.resource('s3')
     bucket = s3.Bucket(bucket)
     objCollection = bucket.objects.all()
     return [obj.key for obj in objCollection]
 
 @click.command(
-    help = "Publishes all objects in an S3 BUCKET to an SNS TOPIC_ARN.")
+    help = "Publishes all objects in a REGION's S3 BUCKET to an SNS TOPIC_ARN.")
+@click.argument("region")
 @click.argument("bucket")
 @click.argument("topic_arn")
-def cli(bucket, topic_arn):
-    click.echo("bucket: " + bucket + " topic_arn: " + topic_arn) 
+def cli(region, bucket, topic_arn):
+    global aws_session
+    click.echo("region: " + region + " bucket: " + bucket + " topic_arn: " + topic_arn)
+    aws_session = boto3.Session(region_name = region)
     publishS3ContentsToTopic(bucket, topic_arn)
 
 def main():
