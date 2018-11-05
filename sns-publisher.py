901cf0e1a66651fd70e0454dcf979a757a036863
Mike Lee <mblee@fbi.gov>
2018-11-05 14:18:57 +0000

    Added support for C2S region

--- sns-publisher.py
+++ sns-publisher.py
@@ -3,8 +3,12 @@ import click
 
 aws_session = None
 
+# Publish objects back to SNS
 def publishS3ContentsToTopic(bucket, topic_arn):
-    snsclient = aws_session.client('sns')
+    if aws_session.region_name == 'us-iso-east-1':
+        snsclient = aws_session.client('sns', endpoint_url = 'https://sns.us-iso-east-1.c2s.ic.gov')
+    else:
+        snsclient = aws_session.client('sns')
     objs = listS3Objects(bucket)
     i = 1
     total = len(objs)
@@ -18,13 +22,21 @@ def publishS3ContentsToTopic(bucket, topic_arn):
         print("Published [" + str(i) + "/" + str(total) + "]: " + obj)
         i += 1
 
+# Read the value of the S3 object as a string
 def readS3Object(bucket, object):
-    s3 = aws_session.resource('s3')
+    if aws_session.region_name == 'us-iso-east-1':
+        s3 = aws_session.resource('s3', endpoint_url = 'https://s3.us-iso-east-1.c2s.ic.gov')
+    else:    
+        s3 = aws_session.resource('s3')
     obj = s3.Object(bucket, object)
     return obj.get()['Body'].read().decode('utf-8')
 
+# List all S3 objects in a given bucket
 def listS3Objects(bucket):
-    s3 = aws_session.resource('s3')
+    if aws_session.region_name == 'us-iso-east-1':
+        s3 = aws_session.resource('s3', endpoint_url = 'https://s3.us-iso-east-1.c2s.ic.gov')
+    else:    
+        s3 = aws_session.resource('s3')
     bucket = s3.Bucket(bucket)
     objCollection = bucket.objects.all()
     return [obj.key for obj in objCollection]
